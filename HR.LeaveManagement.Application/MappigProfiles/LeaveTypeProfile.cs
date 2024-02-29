493cdfa7f89cc11b72bc314c911a023c27a22081
Omar Abdelhamid <Omar.Abdelhamid@geidea.net>
2024-02-29 11:01:40 +0200

    Install AutoMapper and MediatR Packages and Create Features

---
+++ HR.LeaveManagement.Application/MappigProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HRLeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.MappigProfiles
+{
+    public class LeaveTypeProfile : Profile
+    {
+        public LeaveTypeProfile()
+        {
+            CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+        }
+    }
+}
