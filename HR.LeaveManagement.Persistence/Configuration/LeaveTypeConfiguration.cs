d0f73f620956fbd6281134df3238d8cb62bdf825
Omar Abdelhamid <Omar.Abdelhamid@geidea.net>
2024-03-11 11:47:11 +0200

    Add HRDBContext and Configuration

---
+++ HR.LeaveManagement.Persistence/Configuration/LeaveTypeConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using HRLeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.Configuration
+{
+    public class LeaveTypeConfiguration : IEntityTypeConfiguration<LeaveType>
+    {
+        public void Configure(EntityTypeBuilder<LeaveType> builder)
+        {
+            builder.HasData(
+
+                new LeaveType
+                {
+                    Id = 1,
+                    Name = "Vacation",
+                    DefaultDays = 10,
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                }
+             );
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
---
+++ HR.LeaveManagement.Persistence/DatabaseContext/HRDatabaseContext.cs
@@ -0,0 +1,40 @@
+﻿using HRLeaveManagement.Domain;
+using HRLeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.DatabaseContext
+{
+    public class HRDatabaseContext : DbContext
+    {
+        public HRDatabaseContext(DbContextOptions<HRDatabaseContext> options) : base(options) { }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HRDatabaseContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in base.ChangeTracker.Entries<BaseEntity>()
+                .Where(x => x.State == EntityState.Added || x.State == EntityState.Modified))
+            {
+                entry.Entity.DateUpdated = DateTime.UtcNow;
+
+                if (entry.State == EntityState.Added) entry.Entity.DateCreated = DateTime.UtcNow;
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
