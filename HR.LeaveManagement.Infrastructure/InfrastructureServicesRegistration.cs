6e405fac2e91a281056c009429fe6262793c9613
Omar Abdelhamid <Omar.Abdelhamid@geidea.net>
2024-04-11 05:03:32 +0200

    Add EmailSender and Configure it using SendGrid Package

--- HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
+++ HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Models.Email;
+using HR.LeaveManagement.Infrastructure.Email;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 
@@ -8,6 +11,8 @@ public static class InfrastructureServicesRegistration
 {
     public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailSender, EmailSender>();
         return services;
     }
 }
