7bf5c4b8293e8b07844251d7fedbda97f1ec8491
Omar Mohamed <om644123@gmail.com>
2024-05-11 14:43:19 +0300

    Configure UI Blazor Proj

---
+++ HR.LeaveManagement.BlazorUI/Services/leaveTypeService.cs
@@ -0,0 +1,12 @@
+﻿using HR.LeaveManagement.BlazorUI.Contracts;
+using HR.LeaveManagement.BlazorUI.Services.Base;
+
+namespace HR.LeaveManagement.BlazorUI.Services
+{
+    public class leaveTypeService : BaseHttpService, IleaveTypeService
+    {
+        public leaveTypeService(IClient client) : base(client)
+        {
+        }
+    }
+}
